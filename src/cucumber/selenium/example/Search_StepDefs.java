a3bf6f21932dd73bdceb3f397b6dfdf9c247c8b3
Howard Deiner <howard.deiner@deinersoft.com>
2016-04-28 17:28:16 -0400

    Add support for Chrome and IE (as well as working with existing Firefox)

--- src/cucumber/selenium/example/Search_StepDefs.java
+++ src/cucumber/selenium/example/Search_StepDefs.java
@@ -2,6 +2,8 @@ package cucumber.selenium.example;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
@@ -10,18 +12,62 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Search_StepDefs {
-    WebDriver webDriver;
+    static boolean hasHookBeenAdded = false;
+    static String browserInUse = null;
+    static WebDriver webDriver;
     WebDriverWait driverWait;
     int timeoutInSeconds = 30;
 
-    @Given("^I open Google$")
-    public void i_open_Google() throws Throwable {
-        webDriver = new FirefoxDriver();
+    @Before
+    public void setUp() {
+        if(!hasHookBeenAdded) {
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                public void run() {
+                    if (webDriver != null) {
+                        webDriver.close();
+                        webDriver.quit();
+                    }
+                }
+            });
+            hasHookBeenAdded = true;
+        }
+
+    }
+
+    @Given("^I am using \"([^\"]*)\" and open Google$")
+    public void i_am_using_and_open_Google(String browserToUse) throws Throwable {
+        if (browserInUse != null) {
+            if ((!(browserToUse.equals(browserInUse)))) {
+                webDriver.close();
+                webDriver.quit();
+            }
+        }
+
+        if (!(browserToUse.equals(browserInUse))) {
+            if (browserToUse.equals("Firefox")) {
+                webDriver = new FirefoxDriver();
+            }
+
+            if (browserToUse.equals("Chrome")) {
+                System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\lib\\" + "chromedriver.exe");
+                webDriver = new ChromeDriver();
+            }
+
+            if (browserToUse.equals("Internet Explorer")) {
+                System.setProperty("webdriver.ie.driver", System.getProperty("user.dir") + "\\lib\\" + "IEDriverServer.exe");
+                webDriver = new InternetExplorerDriver();
+            }
+
+            browserInUse = browserToUse;
+        }
+
         webDriver.get("http://www.google.com/ncr");  // no country redirect
         WebDriverWait wait = new WebDriverWait(webDriver, timeoutInSeconds);
         wait.until(ExpectedConditions.elementToBeClickable(By.name("q")));
@@ -39,6 +85,9 @@ public class Search_StepDefs {
     public void i_should_see(String arg1) throws Throwable {
         String bodyText = webDriver.findElement(By.tagName("body")).getText();
         assertThat(bodyText, containsString(arg1));
-        webDriver.close();
+    }
+
+    @After
+    public void cleanUp() {
     }
 }
--- src/test/resources/features/search.feature
+++ src/test/resources/features/search.feature
@@ -3,9 +3,31 @@ Feature: Search
   As an information seeker
   I want to be able to search using keywords
 
-  Scenario Outline: Search for keywords that yield results
-    Given I open Google
-    And I query on "<queryText>"
+  Scenario Outline: Search for keywords that yield results in Firefox
+    Given I am using "Firefox" and open Google
+    When I query on "<queryText>"
+    Then I should see "<queryResults>"
+
+    Examples:
+      |queryText     |queryResults                                |
+      |cucumber bdd  |Cucumber is for Behaviour-Driven Development|
+      |Bernie Sanders|Bernie Sanders for President                |
+      |Howard Deiner |Deinersoft, Inc.                            |
+
+  Scenario Outline: Search for keywords that yield results in Edge
+    Given I am using "Chrome" and open Google
+    When I query on "<queryText>"
+    Then I should see "<queryResults>"
+
+    Examples:
+      |queryText     |queryResults                                |
+      |cucumber bdd  |Cucumber is for Behaviour-Driven Development|
+      |Bernie Sanders|Bernie Sanders for President                |
+      |Howard Deiner |Deinersoft, Inc.                            |
+
+  Scenario Outline: Search for keywords that yield results in IE
+    Given I am using "Internet Explorer" and open Google
+    When I query on "<queryText>"
     Then I should see "<queryResults>"
 
     Examples:
