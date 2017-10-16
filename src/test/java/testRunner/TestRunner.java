502d11f4e0930598d9a0556516b09438deaf2eae
Howard Deiner <howard.deiner@deinersoft.com>
2017-10-16 11:37:12 -0400

    Add support for Cucumber Pretty reports.

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -10,9 +10,9 @@ import java.util.Map;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = { "src/test/resources/featureFiles" },
-		glue = { "webDriver", "stepDefinition", "testRunner" },
-		tags = { "~@ignore"},
-		plugin = {"pretty"})
+		glue     = { "webDriver", "stepDefinition", "testRunner" },
+		tags     = { "~@ignore"},
+		plugin   = { "pretty", "html:target/cucumber-reports/cucumber-html-report", "json:target/cucumber-reports/cucumber-json-report.json" })
 public class TestRunner {
 	public static Map<String, String> config;
 	public static Scenario scenario;
